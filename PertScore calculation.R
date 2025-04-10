01b53826eb1c481f5695c800258603d383f922b6
ABChh26 <71133508+ABChh26@users.noreply.github.com>
2025-04-10 11:09:19 +0800

    Update PertScore calculation.R

--- PertScore calculation.R
+++ PertScore calculation.R
@@ -1,4 +1,28 @@
 # PertScore calculate after Differentially expressed analysis
+ttest = function(exp, a, b, c, d){
+  Pvalue = c()
+  FC = c()
+  logFC = c()
+  for(i in 1:nrow(exp)){
+    # un-impute
+    y = try(t.test(as.numeric(exp[i, a:b]), as.numeric(exp[i, c:d ])),silent=FALSE)
+    if('try-error' %in% class(y))
+    {
+      Pvalue = c(Pvalue, NA)
+    }else{
+      y = t.test(as.numeric(exp[i, a:b]), as.numeric(exp[i, c:d ]))
+      Pvalue = c(Pvalue, y$p.value)
+    }
+    pre = rowMeans( exp[i, a:b] , na.rm = TRUE)
+    post = rowMeans( exp[i, c:d ] , na.rm = TRUE)
+    FC =c(FC, pre/post) 
+    logFC = c(logFC, log2(pre/post))
+  }
+  FDR=p.adjust(Pvalue, "BH")
+  out<-cbind(exp, Pvalue, FDR, FC, logFC)
+  out
+}
+
 ttest_out = ttest(dat, a, b,c,d)
 sig = c()
 for (i in 1:nrow(ttest_out)){
